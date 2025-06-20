5b00cd293f13cb79930108baf365cb84aff82ea9
Prashant Raj <pr2125116@gmail.com>
2025-06-20 15:56:36 +0530

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
-
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom/cjs/react-router-dom.min";
 import React from "react";
 import './../styles/App.css';
+import HomePage from "./HomePage";
+import About from "./About";
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
+    <BrowserRouter>
+      <Link to='/'>Home</Link>
+      <br/>
+      <Link to='/about'>About</Link>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/about' component={About} />
+      </Switch>
+    </BrowserRouter>
   )
 }
 
